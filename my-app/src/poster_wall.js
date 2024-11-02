1b89a869c3629493c06c2fcc732a9cf418b8b8c6
SHIRLEY <shirleylau27@gmail.com>
2024-11-02 13:22:42 +0100

    added poster clippings

--- my-app/src/poster_wall.js
+++ my-app/src/poster_wall.js
@@ -3,10 +3,10 @@
 import 'reactjs-popup/dist/index.css';
 import './poster_wall.css';
 import './SpotTheDifference.css';
-import newspaper from './assets/cut_outs/newspaper.png'; // Import your image
-import youtube from './assets/cut_outs/computer.png';     // Import other images as needed
-import instagram from './assets/cut_outs/phone.png';
-import picture from './assets/cut_outs/tablet.png';
+import newspaper from './assets/poster_clippings/newspaper_poster.png'; // Import your image
+import youtube from './assets/poster_clippings/youtube_poster.png';     // Import other images as needed
+import instagram from './assets/poster_clippings/instapost_poster.png';
+import picture from './assets/diff_game_pics/turtle_photo.jpeg';
 
 function PosterWall({ win }) {
   const renderItem = (isAvailable, imagePath, altText) => (
