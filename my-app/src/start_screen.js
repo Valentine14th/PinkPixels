d5fe8842f903263f44ff9d21f963685b5f6b494b
SHIRLEY <shirleylau27@gmail.com>
2024-11-02 01:24:25 +0100

    modified start page

--- my-app/src/start_screen.js
+++ my-app/src/start_screen.js
@@ -1,13 +1,22 @@
 import React from "react";
+import './start_screen.css';
 
 function StartScreen({ onStart }) {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
+      <div className="doorwall-background">
+      <div className="doorwall-content">
+      <div className="centered-container">
       <h1>Welcome to the App!</h1>
-      <p>Click the button below to get started.</p>
+      <p>Hallo!
+Gerade haben deine Freunde geschrieben, dass sie sich gerne treffen wollen. Allerdings zwingt dich deinen Eltern zuerst noch dein Poster für deinen Vortrag fertig zu machen!
+Recherchiere mit den verschiedenen Geräten um mehr über Schildkröten zu erfahren!</p>
       <button onClick={onStart} style={{ padding: "10px 20px", fontSize: "1em" }}>
         Start
       </button>
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
